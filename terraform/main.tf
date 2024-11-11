50bac4ec96d3959987c6c7224633381f0d9e7018
theorjiugovictor <orjiugo.victor@gmail.com>
2024-11-11 13:17:58 +0100

    updated the front end

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,24 +29,11 @@ data "aws_route53_zone" "existing" {
   zone_id = "Z04810281WDJTHR846HVG"
 }
 
-# S3 bucket for website
 resource "aws_s3_bucket" "website" {
   bucket = "theorjiugovictor.com"
 
   lifecycle {
     prevent_destroy = true
-  }
-}
-
-resource "aws_s3_bucket_website_configuration" "website" {
-  bucket = aws_s3_bucket.website.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
   }
 }
 
@@ -57,6 +44,29 @@ resource "aws_s3_bucket_public_access_block" "website" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+# CloudFront Origin Access Identity
+resource "aws_cloudfront_origin_access_identity" "default" {
+  comment = "Access Identity for CloudFront to access S3 bucket"
+}
+
+# Add S3 bucket policy
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontAccess"
+        Effect    = "Allow"
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.default.iam_arn
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.website.arn}/*"
+      }
+    ]
+  })
 }
 
 # Route53 record for the website
