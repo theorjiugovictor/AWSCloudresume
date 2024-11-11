102adff8a1638f7de84c46abbb502c419d1eca53
theorjiugovictor <orjiugo.victor@gmail.com>
2024-11-11 11:58:57 +0100

    Updated the terraform code to fix api issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -154,7 +154,7 @@ resource "aws_lambda_function" "visitor_counter" {
 # API Gateway
 resource "aws_api_gateway_rest_api" "visitor_counter" {
   name        = "visitor-counter-api"
-  description = "API for updating visitor count"
+  description = "API for the visitor counter Lambda function"
 }
 
 resource "aws_api_gateway_resource" "count" {
@@ -163,6 +163,69 @@ resource "aws_api_gateway_resource" "count" {
   path_part   = "count"
 }
 
+# Add CORS support
+resource "aws_api_gateway_method" "options" {
+  rest_api_id   = aws_api_gateway_rest_api.visitor_counter.id
+  resource_id   = aws_api_gateway_resource.count.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "options" {
+  rest_api_id = aws_api_gateway_rest_api.visitor_counter.id
+  resource_id = aws_api_gateway_resource.count.id
+  http_method = aws_api_gateway_method.options.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "options" {
+  rest_api_id = aws_api_gateway_rest_api.visitor_counter.id
+  resource_id = aws_api_gateway_resource.count.id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options" {
+  rest_api_id = aws_api_gateway_rest_api.visitor_counter.id
+  resource_id = aws_api_gateway_resource.count.id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = aws_api_gateway_method_response.options.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
+# Add GET method
+resource "aws_api_gateway_method" "get" {
+  rest_api_id   = aws_api_gateway_rest_api.visitor_counter.id
+  resource_id   = aws_api_gateway_resource.count.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "get" {
+  rest_api_id             = aws_api_gateway_rest_api.visitor_counter.id
+  resource_id             = aws_api_gateway_resource.count.id
+  http_method             = aws_api_gateway_method.get.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.visitor_counter.invoke_arn
+}
+
+# Add POST method
 resource "aws_api_gateway_method" "post" {
   rest_api_id   = aws_api_gateway_rest_api.visitor_counter.id
   resource_id   = aws_api_gateway_resource.count.id
@@ -179,6 +242,48 @@ resource "aws_api_gateway_integration" "lambda" {
   uri                     = aws_lambda_function.visitor_counter.invoke_arn
 }
 
+# Lambda permission for API Gateway
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.visitor_counter.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.visitor_counter.execution_arn}/*/*"
+}
+
+# Deployment and Stage
+resource "aws_api_gateway_deployment" "visitor_counter" {
+  depends_on = [
+    aws_api_gateway_integration.lambda,    # POST integration
+    aws_api_gateway_integration.get,       # GET integration
+    aws_api_gateway_integration.options    # OPTIONS integration
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.visitor_counter.id
+
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.count.id,
+      aws_api_gateway_method.get.id,
+      aws_api_gateway_method.post.id,
+      aws_api_gateway_method.options.id,
+      aws_api_gateway_integration.lambda.id,
+      aws_api_gateway_integration.get.id,
+      aws_api_gateway_integration.options.id,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "dev" {
+  deployment_id = aws_api_gateway_deployment.visitor_counter.id
+  rest_api_id  = aws_api_gateway_rest_api.visitor_counter.id
+  stage_name   = "dev"
+}
+
 # IAM role for Lambda
 resource "aws_iam_role" "lambda_role" {
   name               = "visitor_counter_lambda_role"
@@ -192,4 +297,8 @@ output "website_endpoint" {
 
 output "domain_name" {
   value = "theorjiugovictor.com"
+}
+
+output "api_gateway_url" {
+  value = "${aws_api_gateway_stage.dev.invoke_url}/count"
 }
